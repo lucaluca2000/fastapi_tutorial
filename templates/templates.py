b0f53c39f1ec2824c0626518d6bd949a93187f80
ali momeni <alimomeni2000.official@gmail.com>
2022-02-20 01:53:51 +0330

    Background tasks

--- templates/templates.py
+++ templates/templates.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, BackgroundTasks
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.requests import Request
 from schemas import ProductBase
-
+from custom_log import log
 router = APIRouter(
     prefix='/templates',
     tags=['templates']
@@ -13,7 +13,13 @@ templates = Jinja2Templates(directory="templates")
  
 
 @router.post("/products/{id}", response_class=HTMLResponse)
-def post_product(id: str, product: ProductBase, request:Request):
+def post_product(
+        id: str,
+        product: ProductBase, 
+        request:Request,
+        bt:BackgroundTasks
+        ):
+    bt.add_task(log_template_call,f"tempalate for product with id {id}")
     return templates.TemplateResponse(
             "product.html",
             {
@@ -25,3 +31,6 @@ def post_product(id: str, product: ProductBase, request:Request):
 
             }
         )
+
+def log_template_call(message: str):
+    log("MyAPI", message)
